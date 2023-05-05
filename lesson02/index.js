c56b5b2ca0c42df285314ee878cdbea9fe80050f
tom106gunma <tom106.net@gmail.com>
2023-05-05 22:11:26 +0900

    refactor: deleted unused comments.

--- lesson02/index.js
+++ lesson02/index.js
@@ -1,17 +1,13 @@
 const ul = document.getElementById("js-target");
 
-// li要素を作成
 const li = document.createElement("li");
 
-// img要素を作成
 const img = document.createElement("img");
 img.src = "bookmark.png";
 img.alt = "ブックマーク";
 
-// a要素を作成
 const a = document.createElement("a");
 a.href = "1.html"
 a.textContent = "これです"
 
-// ulの中に差し込む imgはaの先頭に
 ul.appendChild(li).appendChild(a).prepend(img);
