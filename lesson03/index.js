42620fc729a2e049c6d2b67e22360919906aaf56
tom106gunma <tom106.net@gmail.com>
2023-05-08 22:01:32 +0900

    refactor: li added to the created ul

--- lesson03/index.js
+++ lesson03/index.js
@@ -1,24 +1,30 @@
 const jsTarget = document.getElementById("js-target");
+const ul = document.createElement("ul");
+const fragment = document.createDocumentFragment();
 
 const attributes = [
   {
-    href: "1.html",
+    href: "a1.html",
+    text: "a1",
     src: "img/bookmark.png"
   },
   {
-    href: "2.html",
+    href: "a2.html",
+    text: "a2",
     src: "img/message.png"
   }
 ];
 
 attributes.map((attribute) => {
-  const ul = document.createElement("ul")
   const li = document.createElement("li");
   const a = document.createElement("a");
   const img = document.createElement("img");
 
   a.href = attribute.href;
+  a.textContent = attribute.text;
   img.src = attribute.src;
 
-  jsTarget.appendChild(ul).appendChild(li).appendChild(a).appendChild(img);
+  fragment.appendChild(li).appendChild(a).insertAdjacentElement("afterbegin",img);
 });
+
+jsTarget.appendChild(ul).appendChild(fragment);
