03e0abf88d1ff1d40377d860e160b49a219838bc
tom106gunma <tom106.net@gmail.com>
2023-05-31 07:17:24 +0900

    feat: get data using a simple API

---
+++ lesson11/index.js
@@ -0,0 +1,61 @@
+const ul = document.getElementById('js-target');
+
+function addLoading() {
+  const img = document.createElement('img');
+  img.id = 'loading-img';
+  img.src = 'img/loading-circle.gif';
+  img.alt = 'loading';
+  ul.appendChild(img);
+}
+
+function removeLoading() {
+  document.getElementById('loading-img').remove();
+}
+
+async function fetchData() {
+  addLoading();
+  try{
+    const url = 'https://my-json-server.typicode.com/tom106gunma/test-json-data/data';
+    const response = await fetch(url);
+    if (!response.ok) {
+      const errorText = `${response.status}:Failed to acquire data.`;
+      errorHandling(errorText);
+    }
+    const data = await response.json();
+    return data;
+  } catch(error) {
+    errorHandling(error);
+  } finally {
+    removeLoading();
+  }
+}
+
+function errorHandling(error) {
+  const p = document.createElement("p");
+  p.textContent = error;
+  ul.appendChild(p);
+  console.error(error);
+}
+
+async function addList() {
+  const attributes = await fetchData() || {};
+  if(Object.keys(attributes).length == 0) return;
+
+  const fragment = document.createDocumentFragment();
+
+  for(const attribute of attributes) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    const img = document.createElement('img');
+    a.href = `/${attribute.to}`;
+    a.textContent = attribute.text;
+    img.src = attribute.img;
+    img.alt = attribute.a;
+    fragment.appendChild(li).appendChild(a).insertAdjacentElement('afterbegin', img);
+  };
+
+  ul.appendChild(fragment);
+
+}
+
+addList();
